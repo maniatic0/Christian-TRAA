adb5002a4ce31e876a96a28e01fc96272c0a53f4
Christian Oliveros <christianol_01@hotmail.com>
2018-01-30 18:14:09 +0100

    New Folder Structure

--- CG_Labs/assimp/binary/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Christian/Documents/Lund/Project/CG_Labs/assimp/code/contrib/zlib
-# Build directory: C:/Users/Christian/Documents/Lund/Project/CG_Labs/assimp/binary/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- CG_Labs/assimp/binary/revision.h
+++
@@ -1,7 +0,0 @@
-#ifndef ASSIMP_REVISION_H_INC
-#define ASSIMP_REVISION_H_INC
-
-#define GitVersion 0xeb8639d7
-#define GitBranch "HEAD"
-
-#endif // ASSIMP_REVISION_H_INC
