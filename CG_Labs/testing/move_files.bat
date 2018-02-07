a7c33a9787fd19d5c9aaad2a3d190280ab54589d
Christian Oliveros <christianol_01@hotmail.com>
2018-02-07 16:57:51 +0100

    Better test order

--- CG_Labs/testing/move_files.bat
+++ CG_Labs/testing/move_files.bat
@@ -1,4 +1,4 @@
 :: Made by Christian Oliveros on 01/02/2018
 :: Takes all the png's from ../res and moves to a folder with the test name in this folder
-move_files.py ../res .
+move_files.py ../res tests
 @pause
